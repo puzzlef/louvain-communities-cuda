0002d564fc8f68e5fb52c6374f7da36f3f655b53
skarr4616 <souvikkarfa06@gmail.com>
2024-05-14 13:40:15 +0530

    Running only static algorithms

--- main.cxx
+++ main.cxx
@@ -10,9 +10,6 @@
 
 using namespace std;
 
-
-
-
 #pragma region CONFIGURATION
 #ifndef TYPE
 /** Type of edge weights. */
@@ -32,9 +29,6 @@ using namespace std;
 #endif
 #pragma endregion
 
-
-
-
 #pragma region METHODS
 #pragma region HELPERS
 /**
@@ -45,14 +39,13 @@ using namespace std;
  * @returns modularity
  */
 template <class G, class K>
-inline double getModularity(const G& x, const LouvainResult<K>& a, double M) {
-  auto fc = [&](auto u) { return a.membership[u]; };
+inline double getModularity(const G &x, const LouvainResult<K> &a, double M)
+{
+  auto fc = [&](auto u)
+  { return a.membership[u]; };
   return modularityBy(x, fc, M, 1.0);
 }
 #pragma endregion
-
-
-
 
 #pragma region EXPERIMENTAL SETUP
 /**
@@ -62,22 +55,27 @@ inline double getModularity(const G& x, const LouvainResult<K>& a, double M) {
  * @param fn function to run on each batch update
  */
 template <class G, class R, class F>
-inline void runBatches(const G& x, R& rnd, F fn) {
-  using  E = typename G::edge_value_type;
+inline void runBatches(const G &x, R &rnd, F fn)
+{
+  using E = typename G::edge_value_type;
   double d = BATCH_DELETIONS_BEGIN;
   double i = BATCH_INSERTIONS_BEGIN;
-  for (int epoch=0;; ++epoch) {
-    for (int r=0; r<REPEAT_BATCH; ++r) {
-      auto y  = duplicate(x);
-      for (int sequence=0; sequence<BATCH_LENGTH; ++sequence) {
-        auto deletions  = generateEdgeDeletions (rnd, y, size_t(d * x.size()/2), 1, x.span()-1, true);
-        auto insertions = generateEdgeInsertions(rnd, y, size_t(i * x.size()/2), 1, x.span()-1, true, E(1));
+  for (int epoch = 0;; ++epoch)
+  {
+    for (int r = 0; r < REPEAT_BATCH; ++r)
+    {
+      auto y = duplicate(x);
+      for (int sequence = 0; sequence < BATCH_LENGTH; ++sequence)
+      {
+        auto deletions = generateEdgeDeletions(rnd, y, size_t(d * x.size() / 2), 1, x.span() - 1, true);
+        auto insertions = generateEdgeInsertions(rnd, y, size_t(i * x.size() / 2), 1, x.span() - 1, true, E(1));
         tidyBatchUpdateU(deletions, insertions, y);
         applyBatchUpdateOmpU(y, deletions, insertions);
         fn(y, d, deletions, i, insertions, sequence, epoch);
       }
     }
-    if (d>=BATCH_DELETIONS_END && i>=BATCH_INSERTIONS_END) break;
+    if (d >= BATCH_DELETIONS_END && i >= BATCH_INSERTIONS_END)
+      break;
     d BATCH_DELETIONS_STEP;
     i BATCH_INSERTIONS_STEP;
     d = min(d, double(BATCH_DELETIONS_END));
@@ -85,36 +83,36 @@ inline void runBatches(const G& x, R& rnd, F fn) {
   }
 }
 
-
 /**
  * Run a function on each number of threads, for a specific epoch.
  * @param epoch epoch number
  * @param fn function to run on each number of threads
  */
 template <class F>
-inline void runThreadsWithBatch(int epoch, F fn) {
+inline void runThreadsWithBatch(int epoch, F fn)
+{
   int t = NUM_THREADS_BEGIN;
-  for (int l=0; l<epoch && t<=NUM_THREADS_END; ++l)
+  for (int l = 0; l < epoch && t <= NUM_THREADS_END; ++l)
     t NUM_THREADS_STEP;
   omp_set_num_threads(t);
   fn(t);
   omp_set_num_threads(MAX_THREADS);
 }
 
-
 /**
  * Run a function on each number of threads, with a specified range of thread counts.
  * @param fn function to run on each number of threads
  */
 template <class F>
-inline void runThreadsAll(F fn) {
-  for (int t=NUM_THREADS_BEGIN; t<=NUM_THREADS_END; t NUM_THREADS_STEP) {
+inline void runThreadsAll(F fn)
+{
+  for (int t = NUM_THREADS_BEGIN; t <= NUM_THREADS_END; t NUM_THREADS_STEP)
+  {
     omp_set_num_threads(t);
     fn(t);
     omp_set_num_threads(MAX_THREADS);
   }
 }
-
 
 /**
  * Run a function on each number of threads, with a specified range of thread counts or for a specific epoch (depending on NUM_THREADS_MODE).
@@ -122,14 +120,14 @@ inline void runThreadsAll(F fn) {
  * @param fn function to run on each number of threads
  */
 template <class F>
-inline void runThreads(int epoch, F fn) {
-  if (NUM_THREADS_MODE=="with-batch") runThreadsWithBatch(epoch, fn);
-  else runThreadsAll(fn);
+inline void runThreads(int epoch, F fn)
+{
+  if (NUM_THREADS_MODE == "with-batch")
+    runThreadsWithBatch(epoch, fn);
+  else
+    runThreadsAll(fn);
 }
 #pragma endregion
-
-
-
 
 #pragma region PERFORM EXPERIMENT
 /**
@@ -137,41 +135,43 @@ inline void runThreads(int epoch, F fn) {
  * @param x original graph
  */
 template <class G>
-void runExperiment(const G& x) {
+void runExperiment(const G &x)
+{
   using K = typename G::key_type;
   using V = typename G::edge_value_type;
   using W = LOUVAIN_WEIGHT_TYPE;
   random_device dev;
   default_random_engine rnd(dev());
   int repeat = REPEAT_METHOD;
-  double M = edgeWeightOmp(x)/2;
+  double M = edgeWeightOmp(x) / 2;
   // Follow a specific result logging format, which can be easily parsed later.
-  auto glog = [&](const auto& ans, const char *technique, int numThreads, const auto& y, auto M, auto deletionsf, auto insertionsf) {
+  auto glog = [&](const auto &ans, const char *technique, int numThreads, const auto &y, auto M, auto deletionsf, auto insertionsf)
+  {
     printf(
-      "{-%.3e/+%.3e batchf, %03d threads} -> "
-      "{%09.1fms, %09.1fms mark, %09.1fms init, %09.1fms firstpass, %09.1fms locmove, %09.1fms aggr, %.3e aff, %04d iters, %03d passes, %01.9f modularity} %s\n",
-      double(deletionsf), double(insertionsf), numThreads,
-      ans.time, ans.markingTime, ans.initializationTime, ans.firstPassTime, ans.localMoveTime, ans.aggregationTime,
-      double(ans.affectedVertices), ans.iterations, ans.passes, getModularity(y, ans, M), technique
-    );
+        "{-%.3e/+%.3e batchf, %03d threads} -> "
+        "{%09.1fms, %09.1fms mark, %09.1fms init, %09.1fms firstpass, %09.1fms locmove, %09.1fms aggr, %.3e aff, %04d iters, %03d passes, %01.9f modularity} %s\n",
+        double(deletionsf), double(insertionsf), numThreads,
+        ans.time, ans.markingTime, ans.initializationTime, ans.firstPassTime, ans.localMoveTime, ans.aggregationTime,
+        double(ans.affectedVertices), ans.iterations, ans.passes, getModularity(y, ans, M), technique);
   };
   // Get community memberships on original graph (static).
   auto b0 = louvainStaticOmp(x, {5});
   glog(b0, "louvainStaticOmpOriginal", MAX_THREADS, x, M, 0.0, 0.0);
-  #if BATCH_LENGTH>1
+#if BATCH_LENGTH > 1
   vector<K> B2, B3, B4;
   vector<W> VW, CW;
-  #else
-  const auto& B2 = b0.membership;
-  const auto& B3 = b0.membership;
-  const auto& B4 = b0.membership;
-  const auto& VW = b0.vertexWeight;
-  const auto& CW = b0.communityWeight;
-  #endif
+#else
+  const auto &B2 = b0.membership;
+  const auto &B3 = b0.membership;
+  const auto &B4 = b0.membership;
+  const auto &VW = b0.vertexWeight;
+  const auto &CW = b0.communityWeight;
+#endif
   // Get community memberships on updated graph (dynamic).
-  runBatches(x, rnd, [&](const auto& y, auto deletionsf, const auto& deletions, auto insertionsf, const auto& insertions, int sequence, int epoch) {
+  runBatches(x, rnd, [&](const auto &y, auto deletionsf, const auto &deletions, auto insertionsf, const auto &insertions, int sequence, int epoch)
+             {
     double M = edgeWeightOmp(y)/2;
-    #if BATCH_LENGTH>1
+#if BATCH_LENGTH > 1
     if (sequence==0) {
       B2 = b0.membership;
       B3 = b0.membership;
@@ -179,7 +179,7 @@ void runExperiment(const G& x) {
       VW = b0.vertexWeight;
       CW = b0.communityWeight;
     }
-    #endif
+#endif
     // Adjust number of threads.
     runThreads(epoch, [&](int numThreads) {
       auto flog = [&](const auto& ans, const char *technique) {
@@ -190,26 +190,24 @@ void runExperiment(const G& x) {
       flog(b1, "louvainStaticOmp");
       auto c1 = louvainStaticCuda(y, {repeat});
       flog(c1, "louvainStaticCuda");
-      // Find naive-dynamic Louvain.
-      auto b2 = louvainNaiveDynamicOmp(y, deletions, insertions, B2, VW, CW, {repeat});
-      flog(b2, "louvainNaiveDynamicOmp");
-      // Find delta-screening based dynamic Louvain.
-      auto b3 = louvainDynamicDeltaScreeningOmp(y, deletions, insertions, B3, VW, CW, {repeat});
-      flog(b3, "louvainDynamicDeltaScreeningOmp");
-      // Find frontier based dynamic Louvain.
-      auto b4 = louvainDynamicFrontierOmp(y, deletions, insertions, B4, VW, CW, {repeat});
-      flog(b4, "louvainDynamicFrontierOmp");
-      #if BATCH_LENGTH>1
+// Find naive-dynamic Louvain.
+// auto b2 = louvainNaiveDynamicOmp(y, deletions, insertions, B2, VW, CW, {repeat});
+// flog(b2, "louvainNaiveDynamicOmp");
+// Find delta-screening based dynamic Louvain.
+// auto b3 = louvainDynamicDeltaScreeningOmp(y, deletions, insertions, B3, VW, CW, {repeat});
+// flog(b3, "louvainDynamicDeltaScreeningOmp");
+// Find frontier based dynamic Louvain.
+// auto b4 = louvainDynamicFrontierOmp(y, deletions, insertions, B4, VW, CW, {repeat});
+// flog(b4, "louvainDynamicFrontierOmp");
+#if BATCH_LENGTH > 1
       B2 = b2.membership;
       B3 = b3.membership;
       B4 = b4.membership;
       VW = b1.vertexWeight;
       CW = b1.communityWeight;
-      #endif
-    });
-  });
+#endif
+    }); });
 }
-
 
 /**
  * Main function.
@@ -217,19 +215,28 @@ void runExperiment(const G& x) {
  * @param argv argument values
  * @returns zero on success, non-zero on failure
  */
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   using K = uint32_t;
   using V = TYPE;
   install_sigsegv();
-  char *file     = argv[1];
-  bool symmetric = argc>2? stoi(argv[2]) : false;
-  bool weighted  = argc>3? stoi(argv[3]) : false;
+  char *file = argv[1];
+  bool symmetric = argc > 2 ? stoi(argv[2]) : false;
+  bool weighted = argc > 3 ? stoi(argv[3]) : false;
   omp_set_num_threads(MAX_THREADS);
   LOG("OMP_NUM_THREADS=%d\n", MAX_THREADS);
   LOG("Loading graph %s ...\n", file);
   DiGraph<K, None, V> x;
-  readMtxOmpW(x, file, weighted); LOG(""); println(x);
-  if (!symmetric) { x = symmetricizeOmp(x); LOG(""); print(x); printf(" (symmetricize)\n"); }
+  readMtxOmpW(x, file, weighted);
+  LOG("");
+  println(x);
+  if (!symmetric)
+  {
+    x = symmetricizeOmp(x);
+    LOG("");
+    print(x);
+    printf(" (symmetricize)\n");
+  }
   runExperiment(x);
   printf("\n");
   return 0;
