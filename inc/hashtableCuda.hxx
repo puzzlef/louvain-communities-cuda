7f215613412dac907580436a424b8d6ad01ffc07
Subhajit Sahu <wolfram77@gmail.com>
2025-01-12 12:45:03 +0530

    :bug: use hashtable from ν-LPA

--- inc/hashtableCuda.hxx
+++ inc/hashtableCuda.hxx
@@ -46,6 +46,7 @@ inline bool __device__ hashtableAccumulateAtCudU(K *hk, V *hv, size_t i, K k, V 
 /**
  * Accumulate value to an entry in hashtable [device function].
  * @tparam BLOCK called from a thread block?
+ * @tparam HTYPE hashtable type (0: linear, 1: quadratic, 2: double, 3: quadritic + double)
  * @param hk hashtable keys (updated)
  * @param hv hashtable values (updated)
  * @param H capacity of hashtable (prime)
@@ -54,17 +55,25 @@ inline bool __device__ hashtableAccumulateAtCudU(K *hk, V *hv, size_t i, K k, V 
  * @param v value to accumulate
  * @returns whether value was accumulated
  */
-template <bool BLOCK=false, class K, class V>
+template <bool BLOCK=false, int HTYPE=3, class K, class V>
 inline bool __device__ hashtableAccumulateCudU(K *hk, V * hv, size_t H, size_t T, K k, V v) {
-  size_t i = k, di = 1;  // k % T;
-  for (size_t t=0; t<H; ++t, i+=di, di=di*2 + (k % T))
+  size_t i = k, di = 1;
+  for (size_t t=0; t<H; ++t) {
     if (hashtableAccumulateAtCudU<BLOCK>(hk, hv, i % H, k, v)) return true;
+    switch (HTYPE) {
+      case 0: ++i; break;
+      case 1: i += di; di *= 2; break;
+      case 2: i += k % T; break;
+      case 3: i += di; di = di*2 + (k % T); break;
+    }
+  }
   return false;
 }
 
 
 /**
  * Obtain value of an entry in hashtable [device function].
+ * @tparam HTYPE hashtable type (0: linear, 1: quadratic, 2: double, 3: quadritic + double)
  * @param hk hashtable keys
  * @param hv hashtable values
  * @param H capacity of hashtable (prime)
@@ -72,28 +81,19 @@ inline bool __device__ hashtableAccumulateCudU(K *hk, V * hv, size_t H, size_t T
  * @param k entry key
  * @returns entry value
  */
-template <class K, class V>
+template <int HTYPE=3, class K, class V>
 inline V __device__ hashtableGetCud(const K *hk, const V *hv, size_t H, size_t T, K k) {
-  size_t i = k, di = 1;  // k % T;
-  for (size_t t=0; t<H; ++t, i+=di, di=di*2 + (k % T))
+  size_t i = k, di = 1;
+  for (size_t t=0; t<H; ++t) {
     if (hk[i % H]==k) return hv[i % H];
+    switch (HTYPE) {
+      case 0: ++i; break;
+      case 1: i += di; di *= 2; break;
+      case 2: i += k % T; break;
+      case 3: i += di; di = di*2 + (k % T); break;
+    }
+  }
   return V();
-}
-
-
-/**
- * Count non-empty keys in hashtable [device function].
- * @param hk hashtable keys
- * @param H capacity of hashtable (prime)
- * @param i start index
- * @param DI index stride
- */
-template <class K>
-inline size_t __device__ hashtableSizeCud(K *hk, size_t H, size_t i, size_t DI) {
-  size_t a = 0;
-  for (; i<H; i+=DI)
-    if (hk[i] != K()) ++a;
-  return a;
 }
 
 
