4dad691f21b493a901ac3e0769479a0ffce62381
Subhajit Sahu <wolfram77@gmail.com>
2025-01-17 08:50:49 +0530

    :bug: process output logs

--- process.js
+++ process.js
@@ -5,7 +5,7 @@ const path = require('path');
 const ROMPTH = /^OMP_NUM_THREADS=(\d+)/;
 const RGRAPH = /^Loading graph .*\/(.*?)\.mtx \.\.\./m;
 const RORDER = /^order: (\d+) size: (\d+) (?:\[\w+\] )?\{\}/m;
-const RRESLT = /^\{-(.+?)\/\+(.+?) batchf, (.+?) threads\} -> \{(.+?)ms, (.+?)ms preproc, (.+?)ms firstpass, (.+?)ms locmove, (.+?)ms aggr, (.+?) affected, (.+?) iters, (.+?) passes, (.+?) modularity\} (.+)/m;
+const RRESLT = /^\{(.+?) threads\} -> \{(.+?)ms, (.+?)ms mark, (.+?)ms init, (.+?)ms first, (.+?)ms move, (.+?)ms aggr, (.+?) iters, (.+?) passes, (.+?) modularity\} (.+)/m;
 
 
 
@@ -60,17 +60,16 @@ function readLogLine(ln, data, state) {
     state.size  = parseFloat(size);
   }
   else if (RRESLT.test(ln)) {
-    var [, batch_deletions_fraction, batch_insertions_fraction, num_threads, time, preprocessing_time, first_pass_time, local_moving_phase_time, aggregation_phase_time, affected_vertices, iterations, passes, modularity, technique] = RRESLT.exec(ln);
+    const RRESLT = /^\{(.+?) threads\} -> \{(.+?)ms, (.+?)ms mark, (.+?)ms init, (.+?)ms first, (.+?)ms move, (.+?)ms aggr, (.+?) iters, (.+?) passes, (.+?) modularity\} (.+)/m;
+    var [, num_threads, time, marking_time, initialization_time, first_pass_time, local_moving_phase_time, aggregation_phase_time, iterations, passes, modularity, technique] = RRESLT.exec(ln);
     data.get(state.graph).push(Object.assign({}, state, {
-      batch_deletions_fraction:  parseFloat(batch_deletions_fraction),
-      batch_insertions_fraction: parseFloat(batch_insertions_fraction),
       num_threads: parseFloat(num_threads),
       time:        parseFloat(time),
-      preprocessing_time: parseFloat(preprocessing_time),
-      first_pass_time:    parseFloat(first_pass_time),
+      marking_time:            parseFloat(marking_time),
+      initialization_time:     parseFloat(initialization_time),
+      first_pass_time:         parseFloat(first_pass_time),
       local_moving_phase_time: parseFloat(local_moving_phase_time),
       aggregation_phase_time:  parseFloat(aggregation_phase_time),
-      affected_vertices: parseFloat(affected_vertices),
       iterations:  parseFloat(iterations),
       passes:      parseFloat(passes),
       modularity:  parseFloat(modularity),
